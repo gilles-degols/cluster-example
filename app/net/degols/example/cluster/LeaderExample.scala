7b4583a7eb98582cc306888979fe5b268b0d342d
gilles-degols <gilles@degols.net>
2019-01-03 17:22:07 +0100

    Add a custom load balancer

--- app/net/degols/example/cluster/LeaderExample.scala
+++ app/net/degols/example/cluster/LeaderExample.scala
@@ -10,10 +10,12 @@ import javax.inject.{Inject, Singleton}
 import scala.concurrent.duration._
 import com.typesafe.config.{Config, ConfigFactory}
 import net.degols.example.cluster.example.Activity
+import net.degols.example.cluster.loadbalancing.{BasicFilesgateLoadBalancer, BasicFilesgateLoadBalancerType}
+import net.degols.libs.cluster.balancing.{BasicLoadBalancerType, LoadBalancer}
 import net.degols.libs.cluster.{ClusterConfiguration, Tools}
 import net.degols.libs.cluster.core.Cluster
 import net.degols.libs.cluster.manager.{Manager, WorkerLeader}
-import net.degols.libs.cluster.messages.{BasicLoadBalancerType, JVMInstance, WorkerTypeInfo}
+import net.degols.libs.cluster.messages.{JVMInstance, WorkerTypeInfo}
 import play.api.libs.concurrent.InjectedActorSupport
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -23,6 +25,10 @@ import scala.util.Try
 class LeaderExample @Inject()(electionService: ElectionService, configurationService: ConfigurationService, clusterConfiguration: ClusterConfiguration, cluster: Cluster)
   extends WorkerLeader(electionService, configurationService, clusterConfiguration, cluster) with InjectedActorSupport{
   context.system.scheduler.schedule(10 seconds, 10 seconds, self, "DEBUG")
+
+  override protected val userLoadBalancers: List[LoadBalancer] = {
+    List(new BasicFilesgateLoadBalancer())
+  }
 
   private val logger = LoggerFactory.getLogger(getClass)
   override def receive: Receive = {
@@ -44,6 +50,10 @@ class LeaderExample @Inject()(electionService: ElectionService, configurationSer
         worker ! "Super Stuff!"
         //context.system.scheduler.scheduleOnce(30 seconds, worker, Kill)
         worker
+      case "ActivityCustom" =>
+        val worker = context.actorOf(Props.create(classOf[Activity]), name = actorName)
+        worker ! "Super custom stuff!"
+        worker
       case _ => throw new Exception(s"Invalid WorkerTypeId received: $workerTypeId")
     }
   }
@@ -53,7 +63,8 @@ class LeaderExample @Inject()(electionService: ElectionService, configurationSer
     */
   override def allWorkerTypeInfo: List[WorkerTypeInfo] = {
     List(
-      WorkerTypeInfo(self, "Activity", BasicLoadBalancerType(instances = 2, JVMInstance))
+      WorkerTypeInfo(self, "Activity", BasicLoadBalancerType(instances = 2, JVMInstance)),
+      WorkerTypeInfo(self, "ActivityCustom", BasicFilesgateLoadBalancerType(instances = 5, JVMInstance))
     )
   }
 }
---
+++ app/net/degols/example/cluster/loadbalancing/BasicFilesgateLoadBalancer.scala
@@ -0,0 +1,93 @@
+package net.degols.example.cluster.loadbalancing
+
+import net.degols.libs.cluster.balancing.LoadBalancer
+import net.degols.libs.cluster.core.{Node, Worker, WorkerManager, WorkerType}
+import net.degols.libs.cluster.messages._
+import org.slf4j.LoggerFactory
+
+import scala.util.Random
+
+/**
+  * Basic example of a custom load balancer. In this case, we only took the code of the BasicLoadBalancer from the cluster
+  * library and put it in another name to gave a simple example
+  * @param instances
+  * @param instanceType
+  */
+@SerialVersionUID(1L)
+case class BasicFilesgateLoadBalancerType(instances: Int, instanceType: InstanceType = ClusterInstance) extends LoadBalancerType {
+  override def toString: String = {
+    val location = if(instanceType == JVMInstance) "jvm" else "cluster"
+    s"BasicFilesgateLoadBalancer: $instances instances/$location"
+  }
+}
+
+class BasicFilesgateLoadBalancer extends LoadBalancer {
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  override def isLoadBalancerType(loadBalancerType: LoadBalancerType): Boolean = loadBalancerType.isInstanceOf[BasicFilesgateLoadBalancerType]
+
+  override def hardWorkDistribution(workerType: WorkerType): Unit = {
+    logger.debug("BasicFilesgateLoadBalancer - There is no hard work distribution in the BasicFilesgateLoadBalancer.")
+  }
+
+  override def softWorkDistribution(workerType: WorkerType): Unit = {
+    logger.debug(s"BasicFilesgateLoadBalancer - Soft work distribution for ${workerType.id}")
+    val nodes = clusterManagement.cluster.nodesForWorkerType(workerType)
+    val balancerType = workerType.workerTypeInfo.loadBalancerType.asInstanceOf[BasicFilesgateLoadBalancerType]
+
+    if(nodes.isEmpty) {
+      logger.error(s"The WorkerType $workerType has no nodes available, no work distribution possible.")
+    } else {
+      // Depending on the type of WorkType, we want to create a specific number of workers by JVM or per cluster
+      if(balancerType.instanceType == JVMInstance) {
+        softWorkDistributionPerJVM(workerType, nodes, balancerType)
+      } else {
+        softWorkDistributionPerCluster(workerType, nodes, balancerType)
+      }
+    }
+  }
+
+  private def softWorkDistributionPerJVM(workerType: WorkerType, nodes: List[Node], balancerType: BasicFilesgateLoadBalancerType): Unit = {
+    val wantedInstances = balancerType.instances
+
+    nodes.flatMap(_.workerManagers.filter(_.isUp))
+      .foreach(workerManager => {
+        val runningInstances = workerManager.workerTypes.find(_ == workerType).get.workers.filter(_.isUp)
+
+        var i = runningInstances.size
+        if(i < wantedInstances) {
+          logger.info(s"Starting ${wantedInstances - i} instances of $workerType on $this")
+        }
+        while(i < wantedInstances) {
+          workerManager.startWorker(context, workerType)
+          i += 1
+        }
+      })
+  }
+
+  private def softWorkDistributionPerCluster(workerType: WorkerType, nodes: List[Node], balancerType: BasicFilesgateLoadBalancerType): Unit = {
+    val wantedInstances = balancerType.instances
+
+    val managerAndRunningInstances: Map[WorkerManager, List[Worker]] = nodes.flatMap(node => node.workerManagers.filter(_.isUp))
+      .map(workerManager => workerManager -> workerManager.workerTypes.filter(_ == workerType).flatMap(_.workers.filter(_.isUp))).toMap
+    val runningInstances = managerAndRunningInstances.values.flatten.size
+    if(managerAndRunningInstances.keys.isEmpty) {
+      logger.warn(s"There is no WorkerManager available for $workerType, not possible to start the missing ${wantedInstances - wantedInstances} instances.")
+    } else if(runningInstances < wantedInstances) {
+      logger.info(s"Starting ${wantedInstances - runningInstances} instances of $workerType on various WorkerManagers.")
+      // We try to distribute the load between managers. For now we simply choose managers at random (but those having less than the average number of instances)
+      val averageWantedInstances = (wantedInstances + 1f) / managerAndRunningInstances.keys.size
+      val availableManagers = managerAndRunningInstances.toList.filter(_._2.size < averageWantedInstances)
+      if(availableManagers.isEmpty) {
+        logger.error(s"No good WorkerManager found for $workerType...")
+      } else {
+        var i = runningInstances
+        while(i < wantedInstances) {
+          val workerManager = Random.shuffle(availableManagers).head
+          workerManager._1.startWorker(context, workerType)
+          i += 1
+        }
+      }
+    }
+  }
+}

d1989a6a4c3a516839c5278431393daa98e1160e
gilles-degols <gilles@degols.net>
2019-05-05 16:18:50 +0200

    Adapt to the new PackageLeaderApi interface

--- app/net/degols/example/cluster/activity/ActivityLeader.scala
+++ app/net/degols/example/cluster/activity/ActivityLeader.scala
@@ -1,35 +1,26 @@
 package net.degols.example.cluster.activity
 
 import akka.actor.{ActorContext, ActorRef, Props}
-import javax.inject.Inject
+import javax.inject.{Inject, Singleton}
 import net.degols.example.cluster.activity.example.Activity
 import net.degols.example.cluster.activity.loadbalancing.BasicFilesgateLoadBalancerType
 import net.degols.libs.cluster.balancing.BasicLoadBalancerType
 import net.degols.libs.cluster.manager.{PackageLeaderApi, StartWorkerWrapper, WorkerInfo}
 import net.degols.libs.cluster.messages.{JVMInstance, WorkerTypeInfo}
 
+@Singleton
 class ActivityLeader extends PackageLeaderApi {
   override val packageName: String = "Activity"
 
-  override def startWorker(work: StartWorkerWrapper): ActorRef = {
-    work.shortName match {
-      case "Activity" =>
-        val worker = _context.actorOf(Props.create(classOf[Activity]), name = work.actorName)
-        worker ! "Super Stuff!"
-        //context.system.scheduler.scheduleOnce(30 seconds, worker, Kill)
-        worker
-      case "ActivityCustom" =>
-        val worker = _context.actorOf(Props.create(classOf[Activity]), name = work.actorName)
-        worker ! "Super custom stuff!"
-        worker
-      case _ => throw new Exception(s"Invalid short-name received: ${work.shortName}")
-    }
-  }
-
-  override val workerInfos: Seq[WorkerInfo] = {
-    List(
+  override def setupWorkers(): Unit = {
+    setWorker(
       WorkerInfo("Activity", Option(BasicLoadBalancerType(instances = 2, JVMInstance))),
-      WorkerInfo("ActivityCustom", Option(BasicFilesgateLoadBalancerType(instances = 5, JVMInstance)))
+      work => _context.actorOf(Props.create(classOf[Activity]), name = work.actorName)
+    )
+
+    setWorker(
+     WorkerInfo("ActivityCustom", Option(BasicFilesgateLoadBalancerType(instances = 5, JVMInstance))),
+      work => _context.actorOf(Props.create(classOf[Activity]), name = work.actorName)
     )
   }
 }
--- app/net/degols/example/cluster/activity/example/Activity.scala
+++ app/net/degols/example/cluster/activity/example/Activity.scala
@@ -2,10 +2,12 @@ package net.degols.example.cluster.activity.example
 
 import akka.actor.Actor
 import org.slf4j.LoggerFactory
-
+import scala.concurrent.duration._
 
 class Activity extends Actor{
   private val logger = LoggerFactory.getLogger(getClass)
+  implicit val ac = context.system.dispatcher
+  context.system.scheduler.schedule(1 seconds, 10 seconds, self, "ScheduledMessage")
 
   override def receive: Receive = {
     case x =>
